89bbe63d37643808094f67d79795a307b7202863
Zian Pan <zpan@algomau.ca>
2025-04-03 12:13:19 -0400

    css

--- includes/header.php
+++ includes/header.php
@@ -10,14 +10,15 @@ session_start();
   <title>Computer Shop</title>
 
   <!-- CSS -->
-  <link rel="stylesheet" href="/OnlineShop/assets/css/style.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/OnlineShop/css/style.css" />
   <!-- JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="/OnlineShop/js/cart.js"></script>
   <script src="/OnlineShop/js/update.js"></script>
   <script src="/OnlineShop/js/darkmodetoggle.js"></script>
+  
 </head>
 
 <body>
