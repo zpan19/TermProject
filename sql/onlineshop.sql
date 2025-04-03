ec041407da9076d1b40cd81c369ac3b5f8e6a3ed
Zian Pan <zpan@algomau.ca>
2025-04-03 13:58:27 -0400

    add lines that required to add to online.sql

--- sql/onlineshop.sql
+++ sql/onlineshop.sql
@@ -21,6 +21,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+
+
+CREATE DATABASE IF NOT EXISTS `onlineshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `onlineshop`;
 --
 -- Database: `onlineshop`
 --
