803dc48baba5e263d21a44a611ed18013018c779
Zian Pan <zpan@algomau.ca>
2025-04-03 11:44:31 -0400

    finish

--- sql/onlineshop.sql
+++ sql/onlineshop.sql
@@ -7,6 +7,11 @@
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
+
+CREATE DATABASE IF NOT EXISTS `onlineshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `onlineshop`;
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
